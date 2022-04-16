d3a56fb2a96d4ebff77b86779efa4b8da773b813
yeoni sugar <rkdgkdus522@naver.com>
2022-04-17 03:04:46 +0900

    뉴스 타임즈 에러 핸들링 & 페이지네이션 추가
    - 작성자 : 강하연
    - 작성일 : 2022-04-17

--- NEWS_TIMES/main.js
+++ NEWS_TIMES/main.js
@@ -1,4 +1,6 @@
 let news = [];
+let page = 1;
+let totalPages = 0;
 let menus = document.querySelectorAll(".menus button");
 
 menus.forEach((menu) =>
@@ -6,37 +8,53 @@ menus.forEach((menu) =>
 );
 
 let searchButton = document.getElementById("search-button");
+let url; // 전역변수
+
+const getNews = async () => {
+  try {
+    let header = new Headers({
+      "x-api-key": "i7tqjNo6r1w-hCVJkDZ_koWrsUdMTyTSOgGxjy9B5h4",
+    });
+
+    url.searchParams.set("page", page); // &page=${page}
+
+    let response = await fetch(url, { headers: header }); // ajax 이용방법, http이용방법, fetch 이용방법
+    let data = await response.json(); // 위에 데이터를 부르는 것이 끝나고 json을 불러야함
+
+    if (response.status == 200) {
+      if (data.total_hits == 0) {
+        throw new Error("검색된 결과값이 없습니다.");
+      }
+      news = data.articles;
+      totalPages = data.total_pages;
+      page = data.page;
+      render();
+      pagination();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    errorRender(error.message);
+    console.log("잡힌 에러는", error.message);
+  }
+};
 
 // 최근 뉴스 가져오기
 const getLatestNews = async () => {
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business&page_size=10`
   );
-  let header = new Headers({
-    "x-api-key": "i7tqjNo6r1w-hCVJkDZ_koWrsUdMTyTSOgGxjy9B5h4",
-  });
-
-  let response = await fetch(url, { headers: header }); // ajax 이용방법, http이용방법, fetch 이용방법
-  let data = await response.json(); // 위에 데이터를 부르는 것이 끝나고 json을 불러야함
-  news = data.articles;
-  render();
+  getNews();
 };
 
 // 선택한 토픽별로 뉴스 가져오기
 const getNewsByTopic = async (event) => {
+  page = 1;
   let topic = event.target.textContent.toLowerCase();
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`
   );
-
-  let header = new Headers({
-    "x-api-key": "i7tqjNo6r1w-hCVJkDZ_koWrsUdMTyTSOgGxjy9B5h4",
-  });
-
-  let response = await fetch(url, { headers: header }); // ajax 이용방법, http이용방법, fetch 이용방법
-  let data = await response.json(); // 위에 데이터를 부르는 것이 끝나고 json을 불러야함
-  news = data.articles;
-  render();
+  getNews();
 };
 
 // 검색한 키워드 별로 뉴스 가져오기
@@ -49,17 +67,10 @@ const getNewsByKeyword = async () => {
   // 6. data 보여주기
 
   let keyword = document.getElementById("search-input").value;
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
   );
-  let header = new Headers({
-    "x-api-key": "i7tqjNo6r1w-hCVJkDZ_koWrsUdMTyTSOgGxjy9B5h4",
-  });
-
-  let response = await fetch(url, { headers: header }); // ajax 이용방법, http이용방법, fetch 이용방법
-  let data = await response.json(); // 위에 데이터를 부르는 것이 끝나고 json을 불러야함
-  news = data.articles;
-  render();
+  getNews();
 };
 
 const render = () => {
@@ -82,6 +93,76 @@ const render = () => {
     .join("");
 
   document.getElementById("news-board").innerHTML = newsHTML;
+};
+
+const errorRender = (message) => {
+  let errorHTML = `<div class="alert alert-danger text-center" role="alert">
+  ${message}
+</div>`;
+  document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const pagination = () => {
+  // total_page수
+  // 내가 보려는 page
+  // 내가 어떤 그룹에 있는지 page group
+  let pageGroup = Math.ceil(page / 5);
+  // last
+  let last = pageGroup * 5;
+  last = last > totalPages ? totalPages : last;
+  // first
+  let first = last > 4 ? last - 4 : 1;
+  // first~last 페이지 프린트
+
+  let paginationHTML =
+    pageGroup != 1
+      ? `<li class="page-item">
+  <a class="page-link" href="#" aria-label="Previous" onClick="moveToPage(${1})">
+    <span aria-hidden="true">&lt&lt;</span>
+  </a>
+  </li><li class="page-item">
+<a class="page-link" href="#" aria-label="Previous" onClick="moveToPage(${
+          page - 1
+        })">
+  <span aria-hidden="true">&lt;</span>
+</a>
+</li>`
+      : ``;
+
+  // 만약, total page가 3까지 밖에 없을 경우 (3개의 페이지만 프린트 하는법) -> last, first 로직 바궈야함
+  // << >> 이 버튼 만들어 주기(맨처음으로 가기, 맨 끝으로 가는 버튼 만들기)
+  // 내가 그룹 1일때 << < 이 버튼이 없다
+  // 내가 마지막 그룹일 때 > >> 이 버튼이 없다
+
+  for (let i = first; i <= last; i++) {
+    paginationHTML += `<li class="page-item ${
+      page == i ? "active" : ""
+    }"><a class="page-link" href="#" onClick="moveToPage(${i})">${i}</a></li>`;
+  }
+
+  paginationHTML +=
+    pageGroup != Math.ceil(totalPages / 5)
+      ? `<li class="page-item">
+  <a class="page-link" href="#" aria-label="Next" onClick="moveToPage(${
+    page + 1
+  })">
+    <span aria-hidden="true">&gt;</span>
+  </a>
+</li><li class="page-item">
+<a class="page-link" href="#" aria-label="Next" onClick="moveToPage(${totalPages})">
+  <span aria-hidden="true">&gt&gt;</span>
+</a>
+</li>`
+      : ``;
+
+  document.querySelector(".pagination").innerHTML = paginationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  // 1. 이동하고 싶은 페이지를 알아야함
+  page = pageNum;
+  // 2. 이동하고 싶은 페이지를 가지고 api를 다시 호출
+  getNews();
 };
 
 searchButton.addEventListener("click", getNewsByKeyword);
